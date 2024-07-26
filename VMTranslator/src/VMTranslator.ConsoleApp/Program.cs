8768d9f083d1d45bf88fb6f32e38281fe9430bed
Vegard Seines <vegard.seines@outlook.com>
2024-07-26 18:24:00 +0200

    Implement Program

--- VMTranslator/src/VMTranslator.ConsoleApp/Program.cs
+++ VMTranslator/src/VMTranslator.ConsoleApp/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using VMTranslator.Library;
+
+namespace VMTranslator.ConsoleApp;
+
+public class Program
+{
+  public static void Main(string[] args)
+  {
+      string inputFile = args[0];
+      ValidateInput(inputFile);
+
+      string outputFile = inputFile.Replace(".vm", ".asm");
+      string[] assemblyCode = Translator.Translate(
+              File.ReadLines(inputFile).ToArray<string>()
+          );
+
+      using (StreamWriter writer = new(outputFile))
+      {
+          foreach (string line in assemblyCode)
+          {
+              writer.WriteLine(line);    
+          }
+      }
+  }
+  /// <summary>
+  /// Throws exceptions if the input is not valid.
+  /// </summary>
+  /// <exception cref="FileNotFoundException">
+  /// Thrown if the file can not be found.
+  /// </exception>
+  /// <exception cref="ArgumentException">
+  /// Thrown if the file is not a valid vm file.
+  /// </exception>
+  static void ValidateInput(string input)
+  {
+      if (!File.Exists(input))
+      {
+          throw new FileNotFoundException($"The file: {input} could not be found.");
+      }
+      if (!input.Contains(".vm"))
+      {
+          throw new ArgumentException($"The file: {input} is not a valid VM file.");
+      }
+      
+  }
+}
